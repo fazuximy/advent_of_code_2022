4968be2657c003e5f4cfce40fa4130658ec8e3f3
fazuximy <fazuximy12@gmail.com>
2022-12-01 20:37:38 +0100

    Adding solutions for day 1

--- Day_01/day_01.py
+++ Day_01/day_01.py
@@ -15,7 +15,7 @@ class ElfCalories:
     summed_elf_calories: int
 
 
-# Task 1
+# Part 1
 
 """
 The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. 
@@ -42,6 +42,20 @@ for elf_id, elf_calories in enumerate(calorie_list):
     elf_dataclass_list.append(ElfCalories(elf_id,elf_calorie_list,sum(elf_calorie_list)))
     
 
-print(max([dataclass.summed_elf_calories for dataclass in elf_dataclass_list]))
+top_1 = max([dataclass.summed_elf_calories for dataclass in elf_dataclass_list])
     
+print(f"The total calories carried by the Elf that carries the most calories are: {top_1}")
+
+# Part 2
+
+"""
+By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most 
+Calories of food might eventually run out of snacks.
+
+To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top 
+three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
+"""
     
+top_3_summed = sum(sorted([dataclass.summed_elf_calories for dataclass in elf_dataclass_list])[-3:])
+
+print(f"The total calories carried by the top 3 Elves that carries the most calories are: {top_3_summed}")
